938024c7450438c045175ea28bd06f5a4fdda836
Vadim <1strelok3.ru@gmail.com>
2023-05-20 20:48:20 +0300

    testing

--- main.py
+++ main.py
@@ -3,6 +3,76 @@ from telebot import types
 
 bot = telebot.TeleBot('6288331035:AAHbLOrsOzsh461YLODeUg-RpG3rpgnYBBc')
 
+import pandas as pd
+data_reviews = pd.read_csv('C:\Users\мвм\Downloads\ext_for_ML.csv')
+data_reviews = data_reviews.drop(columns=["id"])
+
+
+import nltk
+import re
+from nltk.corpus import stopwords
+
+lemmatize = nltk.WordNetLemmatizer()
+import pymorphy2
+from nltk.corpus import stopwords
+
+morph = pymorphy2.MorphAnalyzer()
+ru_stopwords = stopwords.words('russian')
+new_text = []
+for i in data_reviews.text:
+    # удаляем неалфавитные символы
+    text = re.sub("[^a-zA-Zа-яА-Я]", " ", i)
+
+    # токенизируем слова
+    text = nltk.word_tokenize(text, language="russian")
+
+    # лемматирзируем слова
+    text = [morph.normal_forms(word) for word in text if (word not in ru_stopwords)]
+
+    # соединяем слова
+    strin = ""
+    for i in text:
+        for j in i:
+            strin = strin + " " + j
+    new_text.append(strin)
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+count = CountVectorizer()
+# проводим преобразование текста
+matrix = count.fit_transform(new_text).toarray()
+
+
+X=matrix
+y = data_reviews["target"].values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
+
+
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+result_logreg = logreg.fit(x_train, y_train)
+logreg.score(x_test,y_test)
+
+
+def emotional_coloring(text):
+    test_text_1 = re.sub("[^a-zA-Zа-яА-Я]"," ", text)
+    test_text_2 = nltk.word_tokenize(test_text_1,language = "russian")
+    test_text_3 = [morph.normal_forms(word) for word in test_text_2 if (word not in ru_stopwords)]
+    strin_2 = ""
+    for i in test_text_3:
+        for j in i:
+            strin_2 = strin_2 + " " + j
+    new_list = []
+    new_list.append(strin_2)
+    new = count.transform(new_list).toarray()
+    result = logreg.predict(new)
+    if result[0] == 0:
+        return "злой текст:("
+    elif result[0] == 1:
+        return "добрый текст:)"
+
 
 @bot.message_handler(commands=['start'])
 def star(message):
@@ -20,13 +90,11 @@ def website(message):
 
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
-    if message.text == "Hello" or message.text == "hello" or message.text == "hi":
-        bot.send_message(message.chat.id, "Hello to you too!", parse_mode='html')
-    elif message.text == "id":
-        bot.send_message(message.chat.id, f"Your id is {message.from_user.id}", parse_mode='html')
+    result = emotional_coloring(message.text)
+    if result == "злой текст:(":
+        bot.send_message(message.chat.id, "Ваш текст отрицательного содержания", parse_mode='html')
     else:
-        bot.send_message(message.chat.id, "Unfortunately, I am not yet able to carry out the tone of your entered text.", parse_mode='html')
+        bot.send_message(message.chat.id, "Ваш текст положительного содержания", parse_mode='html')
 
 
 bot.polling(none_stop=True)
-
