1ecb8c7947140e27c11ecc8d5ef90938a6d7c031
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-05-22 17:16:00 +0300

    user interaction refactored

--- bot.py
+++ bot.py
@@ -9,26 +9,41 @@ bot = telebot.TeleBot(BOT_TOKEN)
 model = Model(verbose=VERBOSE_LOGS)
 
 
+def get_user_name(message) -> str:
+    if message.from_user.first_name is not None:
+        username = ""
+        username += message.from_user.first_name
+
+        if message.from_user.last_name is not None:
+            username += f" {message.from_user.last_name}"
+
+        return username
+
+    return message.from_user.username
+
+
 @bot.message_handler(commands=['start'])
-def star(message):
-    mess = f'Hello, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
-    bot.send_message(message.chat.id, mess, parse_mode='html')
+def start(message):
+    response = f"""
+Привет, <b>{get_user_name(message)}</b>!
+Я могу определить тональность текста. Напиши мне сообщение, а я определю положительно оно окрашено или нет
+"""
 
-
-@bot.message_handler(commands=['inf'])
-def website(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Find out information about me", url="https://ru.wikipedia.org/wiki/Анализ_"
-                                                                               "тональности_текста"))
-    bot.send_message(message.chat.id, 'I am a telegram bot that can parse text', reply_markup=markup)
+    markup.add(types.InlineKeyboardButton(
+        "Узнать больше про анализ тональности текста",
+        url="https://ru.wikipedia.org/wiki/Анализ_тональности_текста")
+    )
+
+    bot.send_message(message.chat.id, response, reply_markup=markup, parse_mode='html')
 
 
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
     if model.is_emotionally_positive(message.text):
-        bot.send_message(message.chat.id, "Ваш текст положительного содержания", parse_mode='html')
+        bot.send_message(message.chat.id, "Ваш текст <b>положительного</b> содержания", parse_mode='html')
     else:
-        bot.send_message(message.chat.id, "Ваш текст отрицательного содержания", parse_mode='html')
+        bot.send_message(message.chat.id, "Ваш текст <b>отрицательного</b> содержания", parse_mode='html')
 
 
 bot.polling(none_stop=True)
